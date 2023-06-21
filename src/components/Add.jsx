adad27a47c26af4faa6a0c667b78778b5eb4bd79
asd <asd.it@gmail.com>
2023-06-21 22:08:52 +0330

    Fix add component s error

--- src/components/Add.jsx
+++ src/components/Add.jsx
@@ -11,11 +11,9 @@ const Add = () => {
           left: { xs: "calc(50% - 27px ) ", md: 25 },
         }}
       >
-        <IconButton>
           <Fab color="primary" aria-label="add" size={"medium"}>
             <AddIcon />
           </Fab>
-        </IconButton>
       </Tooltip>
     </>
   );
